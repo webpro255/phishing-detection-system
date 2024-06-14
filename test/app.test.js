f4f67e02cfb324c99b02d6d4cfe28cb64c001296
webpro255 <webpro255@gmail.com>
2024-06-14 10:03:42 -0400

    Update app.test.js

--- test/app.test.js
+++ test/app.test.js
@@ -1,7 +1,7 @@
 const request = require('supertest');
 const express = require('express');
-
 const app = express();
+
 app.get('/', (req, res) => {
     res.send('Hello World, this is the phishing detection system!');
 });
